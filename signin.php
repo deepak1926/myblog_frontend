d5d72df61b8ce845a8f77cae82874c5f520539cc
Deepak Satankar <deepak.satankar@outlook.com>
2023-07-20 13:02:27 +0530

    Fronted part upload

---
+++ signin.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive Multipage Blog Website</title>
+    <!-- CUSTOM CSS -->
+    <link rel="stylesheet" href="./style.css">
+    <!-- ICONSOUT CSS -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <!-- GOOGLE FONT(MONTSERRAT) -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Baloo+Bhai+2&family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,800;1,900&family=Poppins:ital@1&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+<section class="form_section">
+    <div class="container form_section-container">
+        <h2>Sign In</h2>
+        <div class="alert_message success">
+            <p>This is an success message</p>
+        </div>
+        <form action="" enctype="multipart/form-data">
+            <input type="text" name="" id="" placeholder="Username or Email">
+            <input type="password" name="" id="" placeholder="Password">
+            
+            <button type="submit" class="btn">Sign In</button>
+            <small>Don't have account? <a href="./signup.html">Sign Up</a></small>
+        </form>
+    </div>
+</section>
+</body></html>
---
+++ signup.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive Multipage Blog Website</title>
+    <!-- CUSTOM CSS -->
+    <link rel="stylesheet" href="./style.css">
+    <!-- ICONSOUT CSS -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <!-- GOOGLE FONT(MONTSERRAT) -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Baloo+Bhai+2&family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,800;1,900&family=Poppins:ital@1&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+<section class="form_section">
+    <div class="container form_section-container">
+        <h2>Sign Up</h2>
+        <div class="alert_message error">
+            <p>This is an error message</p>
+        </div>
+        <form action="" enctype="multipart/form-data">
+            <input type="text" name="" id="" placeholder="First Name">
+            <input type="text" name="" id="" placeholder="Last Name">
+            <input type="text" name="" id="" placeholder="Username">
+            <input type="email" name="" id="" placeholder="Email">
+            <input type="password" name="" id="" placeholder="Create Password">
+            <input type="password" name="" id="" placeholder="Confirm Password">
+            <div class="form_control">
+                <label for="avatar">User Avatar</label>
+                <input type="file" id="avatar" >
+            </div>
+            <button type="submit" class="btn">Sign Up</button>
+            <small>Already have an account? <a href="./signin.html">Sign in</a></small>
+        </form>
+    </div>
+</section>
+</body></html>
